9f8d21b8c0d4710d7fd478a2325590fbbb605c3e
Titus Wormer <tituswormer@gmail.com>
2021-07-22 15:52:21 +0200

    Add `strict` to `tsconfig.json`

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import {convertElement} from 'hast-util-is-element'
  * Check if a node is an embedded element.
  * @type {AssertEmbedded}
  */
-// @ts-ignore Sure, the assertion matches.
+// @ts-expect-error Sure, the assertion matches.
 export const embedded = convertElement([
   'audio',
   'canvas',

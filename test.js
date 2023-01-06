d81cdd4ba1459e6300e5909ff776492bb6aaa674
Titus Wormer <tituswormer@gmail.com>
2023-01-06 14:42:09 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,24 +1,25 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {embedded} from './index.js'
 
-test('embedded', (t) => {
-  t.equal(embedded(), false, 'should return `false` without node')
+test('embedded', () => {
+  assert.equal(embedded(), false, 'should return `false` without node')
 
-  t.equal(embedded(null), false, 'should return `false` with `null`')
+  assert.equal(embedded(null), false, 'should return `false` with `null`')
 
-  t.equal(
+  assert.equal(
     embedded({type: 'text'}),
     false,
     'should return `false` when without `element`'
   )
 
-  t.equal(
+  assert.equal(
     embedded({type: 'element'}),
     false,
     'should return `false` when with invalid `element`'
   )
 
-  t.equal(
+  assert.equal(
     embedded({
       type: 'element',
       tagName: 'a',
@@ -29,7 +30,7 @@ test('embedded', (t) => {
     'should return `false` when without not embedded'
   )
 
-  t.equal(
+  assert.equal(
     embedded({
       type: 'element',
       tagName: 'audio',
@@ -39,6 +40,4 @@ test('embedded', (t) => {
     true,
     'should return `true` when with embedded'
   )
-
-  t.end()
 })
